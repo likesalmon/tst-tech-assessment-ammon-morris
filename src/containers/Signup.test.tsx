e2d3283bfbe42cf521a624bc688cd1adc16a5044
Ammon Morris <likesalmon@gmail.com>
2020-08-03 08:04:33 -0600

    test(Signup): complete stubbed tests

--- src/containers/Signup.test.tsx
+++ src/containers/Signup.test.tsx
@@ -16,18 +16,95 @@ describe("<Signup />", () => {
     expect(spy).not.toHaveBeenCalled();
   });
 
-  it.todo("should persist username");
-  it.todo("should persist password");
-  it.todo("should persist confirm-password");
-  it.todo("should focus on username on load");
-  it.todo("should disable the submit button if the form is empty");
-  it.todo("should disable the submit button if a username is missing");
-  it.todo("should disable the submit button if a password is missing");
-  it.todo("should disable the submit button if a confirm password is missing");
-  it.todo(
-    "should disable the submit button and show an error if password fields don't match"
-  );
-  it.todo(
-    "should not show an error if password has a value but confirm password is empty"
-  );
+  it("should persist username", () => {
+    const { getByLabelText } = render(<Signup />);
+    const input = getByLabelText(/username/i);
+    const value = "foo";
+    fireEvent.change(input, { target: { value } });
+    expect(input).toHaveValue(value);
+  });
+
+  it("should persist password", () => {
+    const { getByLabelText } = render(<Signup />);
+    const input = getByLabelText(/^password/i);
+    const value = "foo";
+    fireEvent.change(input, { target: { value } });
+    expect(input).toHaveValue(value);
+  });
+
+  it("should persist confirm-password", () => {
+    const { getByLabelText } = render(<Signup />);
+    const input = getByLabelText(/^confirm password/i);
+    const value = "foo";
+    fireEvent.change(input, { target: { value } });
+    expect(input).toHaveValue(value);
+  });
+
+  it("should focus on username initially", () => {
+    const { getByLabelText } = render(<Signup />);
+    expect(getByLabelText(/^username/i)).toHaveFocus();
+  });
+
+  it("should disable the submit button if the form is empty", () => {
+    const { getByTestId } = render(<Signup />);
+    expect(getByTestId("submit")).toBeDisabled();
+  });
+
+  it("should disable the submit button if username is empty", () => {
+    const { getByLabelText, getByTestId } = render(<Signup />);
+    const pass = "secret";
+    fireEvent.change(getByLabelText(/^password/i), { target: { value: pass } });
+    fireEvent.change(getByLabelText(/^confirm password/i), {
+      target: { value: pass },
+    });
+    expect(getByTestId("submit")).toBeDisabled();
+  });
+
+  it("should disable the submit button if password is empty", () => {
+    const { getByLabelText, getByTestId } = render(<Signup />);
+    fireEvent.change(getByLabelText(/^username/i), {
+      target: { value: "some user" },
+    });
+    fireEvent.change(getByLabelText(/^confirm password/i), {
+      target: { value: "secret" },
+    });
+    expect(getByTestId("submit")).toBeDisabled();
+  });
+
+  it("should disable the submit button if confirm password is empty", () => {
+    const { getByLabelText, getByTestId } = render(<Signup />);
+    fireEvent.change(getByLabelText(/^username/i), {
+      target: { value: "some user" },
+    });
+    fireEvent.change(getByLabelText(/^password/i), {
+      target: { value: "secret" },
+    });
+    expect(getByTestId("submit")).toBeDisabled();
+  });
+
+  it("should disable the submit button and show an error if password fields don't match", () => {
+    const { getByLabelText, getByTestId, getByText } = render(<Signup />);
+    fireEvent.change(getByLabelText(/^username/i), {
+      target: { value: "some user" },
+    });
+    fireEvent.change(getByLabelText(/^password/i), {
+      target: { value: "secret" },
+    });
+    fireEvent.change(getByLabelText(/^confirm password/i), {
+      target: { value: "foo" },
+    });
+    expect(getByTestId("submit")).toBeDisabled();
+    expect(getByText(/passwords must match/i)).toBeInTheDocument();
+  });
+
+  it("should not show an error if password has a value but confirm password is empty", () => {
+    const { getByLabelText, queryByText } = render(<Signup />);
+    fireEvent.change(getByLabelText(/^username/i), {
+      target: { value: "some user" },
+    });
+    fireEvent.change(getByLabelText(/^password/i), {
+      target: { value: "secret" },
+    });
+    expect(queryByText(/passwords must match/i)).toBeNull();
+  });
 });
